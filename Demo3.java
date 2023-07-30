82765f4b1121f9fb45595d2173ea41604e36db68
Sudam Shaluka <rkshaluka@gmail.com>
2023-07-30 18:44:43 +0530

    Completion task 3,

---
+++ Demo3.java
@@ -0,0 +1,9 @@
+public class Demo3{
+    public static void main(String[] args) {
+        int areaCode = 123 ;
+        int numberMidddle = 456 ;
+        int numberLast = 7890 ;
+        
+        System.out.printf("(\033[35m%d\033[0m) %d-%d",areaCode,numberMidddle,numberLast);
+    }
+}
