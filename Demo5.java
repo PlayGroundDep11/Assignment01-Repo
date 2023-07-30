be1b7b28de01de1c9d668db05a77b7dcaafce1e1
Sudam Shaluka <rkshaluka@gmail.com>
2023-07-30 19:48:45 +0530

    completion Task 5

---
+++ Demo5.java
@@ -0,0 +1,9 @@
+public class Demo5{
+    public static void main(String[] args) {
+        int num1 = 978 ;
+        int num2 = 916 ;
+        int num3 = 1484100 ;
+
+        System.out.printf("%s%s%s-%s%s%3$s-%s%s","\033[32m \n " ,num1,"\033[0m","\033[33m",num2,"\033[34m",num3);
+    }
+}
