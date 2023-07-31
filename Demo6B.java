3f702842f9715ed8a9d238366b8536dafd7b5a51
Sudam Shaluka <rkshaluka@gmail.com>
2023-07-31 11:21:47 +0530

    Task completion 6A,,

--- Demo6B.java
+++ Demo6B.java
@@ -1,11 +1,12 @@
 public class Demo6B{
     public static void main(String[] args) {
-        String coloString = "\033[32m" ;
+        String coloString = "\033[35m" ;
+        String reset = "\033[0m" ;
         System.out.println("+"+"-".repeat(10)+"+"+"-".repeat(5)+"+");
         System.out.printf("|\033[1;36m%-10s\033[0m|\033[1;36m%-5s\033[0m| \n","NAME","AGE");
         System.out.println("+"+"-".repeat(10)+"+"+"-".repeat(5)+"+");
-        System.out.printf("|%-10s|%-5s| \n","Alice","24");
-        System.out.printf("|%-10s|%-5s| \n","Bob","30");
+        System.out.printf("|%-10s|%s%-5s%s| \n","Alice",coloString,"24",reset);
+        System.out.printf("|%-10s|%s%-5s%s| \n","Bob",coloString,"30",reset);
         System.out.println("+"+"-".repeat(10)+"+"+"-".repeat(5)+"+");
     }
 }
