9d5ce7e03d48587591f42ff7b8dbf4bc01f05279
Sudam Shaluka <rkshaluka@gmail.com>
2023-07-31 11:10:40 +0530

    Completion Task 6B

---
+++ Demo6B.java
@@ -0,0 +1,10 @@
+public class Demo6B{
+    public static void main(String[] args) {
+        System.out.println("+"+"-".repeat(10)+"+"+"-".repeat(5)+"+");
+        System.out.printf("|%-10s|%-5s| \n","NAME","AGE");
+        System.out.println("+"+"-".repeat(10)+"+"+"-".repeat(5)+"+");
+        System.out.printf("|%-10s|%-5s| \n","Alice","24");
+        System.out.printf("|%-10s|%-5s| \n","Bob","30");
+        System.out.println("+"+"-".repeat(10)+"+"+"-".repeat(5)+"+");
+    }
+}
